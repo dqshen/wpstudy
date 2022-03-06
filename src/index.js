c70b8849b4f44e6d771c8a0b676d6e7138b07538
dqshen <dqshen@hogwarts>
2022-03-06 21:31:45 +0800

    关于destructuring assignment注释的更正

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import printMe from "./print";
 
 async function getComponent() {
   const element = document.createElement('div');
-  const { default: _ } = await import('lodash');/* 这里用destructure assignment把import的结果 _ 赋值给default */
+
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+  const { default: _ } = await import('lodash');/* 这里用destructuring assignment把import的结果default重新命名为_, 语法问题详见上面的链接*/
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
  
